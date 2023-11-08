9529536ec5eeaa6a35d41d20444df833a836ca3b
YamiYume <p.steven.t.g@gmail.com>
2023-11-07 19:49:35 -0500

    added title

--- animation.py
+++ animation.py
@@ -1,8 +1,15 @@
 from manim import *
+from manim_data_structures import *
 
-
-class CreateCircle(Scene):
+class TitleScene(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        title = Tex(r"Algoritmo Spigot $\pi$", font_size=80)
+        name1 = Tex(r"Salinas Rico, Sofia", font_size=50).shift(1 * DOWN)
+        name2 = Tex(r"Rodríguez Rubio, Juan Antonio", font_size=50).shift(1.5 * DOWN)
+        name3 = Tex(r"Torres Gutierrez, Pawell Steven", font_size=50).shift(2 * DOWN)
+        self.play(Create(title))
+        self.wait(1.5)
+        self.play(Create(name1))
+        self.play(Create(name2))
+        self.play(Create(name3))
+        self.wait(1.5)
